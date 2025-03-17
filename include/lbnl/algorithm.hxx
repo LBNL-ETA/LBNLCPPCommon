9098e28a4f09a04a63f06c11683629ad2096423b
Simon Vidanovic <DVVidanovic@lbl.gov>
2025-03-17 12:32:20 -0700

    Contains and unque added.

--- include/lbnl/algorithm.hxx
+++ include/lbnl/algorithm.hxx
@@ -4,25 +4,50 @@
 #include <ranges>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 // Do not create implementation file. This is header only library and
 
-namespace lbnl 
+namespace lbnl
 {
-    template<typename Container, std::predicate<const typename Container::value_type&> Predicate>
-    std::optional<typename Container::value_type> findElement(const Container& elements, Predicate predicate) {
+    template<typename Container, std::predicate<const typename Container::value_type &> Predicate>
+    std::optional<typename Container::value_type> findElement(const Container & elements,
+                                                              Predicate predicate)
+    {
         const auto it = std::ranges::find_if(elements, predicate);
-        return (it != std::ranges::cend(elements)) ? std::optional<typename Container::value_type>(*it) : std::nullopt;
+        return (it != std::ranges::cend(elements))
+                 ? std::optional<typename Container::value_type>(*it)
+                 : std::nullopt;
+    }
+
+    //! Check if the container has a value
+    template<typename Container, typename T>
+    bool contains(const Container & elements, const T & value)
+    {
+        return std::ranges::find(elements, value) != std::ranges::cend(elements);
+    }
+
+    //! Remove duplicates from a container
+    template<std::ranges::range R>
+    auto unique(const R& range)
+    {
+        using ValueType = std::ranges::range_value_t<R>;
+
+        std::vector<ValueType> result(range.begin(), range.end());
+        std::sort(result.begin(), result.end());
+        result.erase(std::unique(result.begin(), result.end()), result.end());
+
+        return result;
     }
 
     inline std::vector<std::string> split(std::string_view str, char delimiter)
     {
         std::vector<std::string> result;
-        for (auto part : str | std::views::split(delimiter))
+        for(auto part : str | std::views::split(delimiter))
         {
             result.emplace_back(part.begin(), part.end());
         }
         return result;
     }
 
-}  // namespace lbnl
+}   // namespace lbnl
